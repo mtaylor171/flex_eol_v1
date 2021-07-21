31108b93d1b32869c900ffadbe8495498703647f
Matt <mtaylor@nuroai>
2021-07-21 12:44:52 -0700

    rms every 0.5s

--- motor/motor_main.py
+++ motor/motor_main.py
@@ -210,7 +210,7 @@ class MotorController(object):
                 msg = "STALL DETECTED"
                 return 0, msg
         
-        if(len(self.data[0] > 2)) and (temp_data[0] - self.data[0][self.last_current_index - 1] >= 100000):
+        if(len(self.data[0]) > 2) and (temp_data[0] - self.data[0][self.last_current_index - 1] >= 100000):
             self._calculate_rms(self.last_current_index - 1, (len(self.data[0]) - 1))
             self.last_current_index = (len(self.data[0]))
             self.csv_data.insert(1, round(self.freq, 1))
